febda63b37c45726f3b0a62536815f4e1be18f0c
arsatija <64511217+arsatija@users.noreply.github.com>
2021-12-12 23:24:32 -0500

    Added new sounds

--- sound.js
+++ sound.js
@@ -1,6 +1,9 @@
 var audio1 = new Audio('./Fuck_no_baby.mp3');
 var audio2 = new Audio('./Oh_hell_nah.mp3');
 var audio3 = new Audio('./Oh_my_god.mp3');
+var audio4 = new Audio('./Fuck_you.mp3');
+var audio5 = new Audio('./Get_me_the_fuck_outta_there.mp3');
+var audio6 = new Audio('./Oh_shit.mp3');
 
 var buttonFuckNo = document.getElementById('button1');
 buttonFuckNo.addEventListener('click', function() {
@@ -13,4 +16,16 @@ hellNah.addEventListener('click', function() {
 var ohMyGod = document.getElementById('button3');
 ohMyGod.addEventListener('click', function() {
 	audio3.play();
+});
+var fuckYou = document.getElementById('button4');
+fuckYou.addEventListener('click', function() {
+	audio4.play();
+});
+var getMeOut = document.getElementById('button5');
+getMeOut.addEventListener('click', function() {
+	audio5.play();
+});
+var ohShit = document.getElementById('button6');
+ohShit.addEventListener('click', function() {
+	audio6.play();
 });
